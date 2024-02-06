7cd64262a23efb4652b60628113c9b2d1e9277a7
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-05 20:49:13 -0600

    Added jobRoleSelect event listener
    'Other job role' input now only displays if the 'other' option is selected in the select menu

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
+const nameField = document.querySelector('input[type="text"]');
+const otherJobInput = document.querySelector("#other-job-role");
+const jobRoleSelect = document.querySelector('#title');
+
 //Set focus on first form field
-document.querySelector('input[type="text"]').focus();
+nameField.focus();
+
+//Hide 'Other' job role input until 'Other' is selected in dropdown
+otherJobInput.style.display = 'none';
+
+
+
+jobRoleSelect.addEventListener('change', e => {
+    const jobSelection = jobRoleSelect.value;
+    //console.log(jobSelection);
+    if (jobSelection === 'other') {
+        otherJobInput.style.display = 'inherit';
+    } else {
+        otherJobInput.style.display = 'none';
+    }
+});
+
+//Program the "Job Role" <select> element to listen for changes.
+// When a change is detected, display/hide the "text field" based on the selection in the drop-down menu.
