c7fef4c340c10eae1975c96b6bb61bdbb76ecb70
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-05 23:58:04 -0600

    Added color select menu event/logic
    Shirt now only displays valid color options and sets colors value to a valid color option

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const nameField = document.querySelector('input[type="text"]');
-const otherJobInput = document.querySelector("#other-job-role");
-const jobRoleSelect = document.querySelector('#title');
+const nameField = document.getElementById('name');
+const otherJobInput = document.getElementById('other-job-role');
+const jobRoleSelect = document.getElementById('title');
+const colorSelect = document.getElementById('color');
 
-//Set focus on first form field
+//Set focus on first form field && disable color select menu
 nameField.focus();
+colorSelect.disabled = true;
 
 //Hide 'Other' job role input until 'Other' is selected in dropdown
 otherJobInput.style.display = 'none';
@@ -20,5 +22,30 @@ jobRoleSelect.addEventListener('change', e => {
     }
 });
 
-//Program the "Job Role" <select> element to listen for changes.
-// When a change is detected, display/hide the "text field" based on the selection in the drop-down menu.
+
+const designMenu = document.querySelector('#design');
+
+designMenu.addEventListener('change', e => {
+    colorSelect.disabled = false;
+    const shirtSelection = e.target.value;
+    const shirtColors = [...colorSelect.children];
+
+    const matchingShirtColors = shirtColors.filter( color => {
+        if (color.dataset.theme !== shirtSelection){
+            color.style.display = 'none';
+        } else {
+            color.style.display = 'block';
+            return color;
+        }
+    });
+    matchingShirtColors[0].selected = true;
+});
+/**
+ * Disable the "Color" <select> element.
+    Set up the "Design" <select> element to listen for changes. When a change is detected:
+        The "Color" <select> element should be enabled.
+        The "Color" <select> element should display an available color.
+        The "Color" dropdown menu should display only the color options associated with the selected design. For example:
+        If the user selects "Theme - JS Puns" then the "Color" menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+        If the user selects "Theme - I ♥ JS" then the "Color" menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+ */
