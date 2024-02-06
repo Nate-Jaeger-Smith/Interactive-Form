09edb3a7216f8a249b27a32d2222456ad263ab4d
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-06 01:52:36 -0600

    Created activityRegister eventListener
    The total cost of activities now updates in the activities-cost of the fieldset. Will add or subtract totals depending on if activity is being checked or unchecked

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const colorSelect = document.getElementById('color');
 const otherJobInput = document.getElementById('other-job-role');
 const jobRoleSelect = document.getElementById('title');
 const designMenu = document.querySelector('#design');
+const activityRegister = document.getElementById('activities');
+const activitiesTotal = document.getElementById('activities-cost');
 
 //Set focus on first form field && disable color select menu
 nameField.focus();
@@ -29,6 +31,7 @@ jobRoleSelect.addEventListener('change', () => {
 /**
  * Event listener for the change event on the designMenu element
  * Toggles 'hidden' on options that are not valid color matches
+ * Sets color select element to display first 
  * @param {event} e - The event object
  */
 designMenu.addEventListener('change', e => {
@@ -45,4 +48,22 @@ designMenu.addEventListener('change', e => {
         }
     });
     matchingShirtColors[0].selected = true;
+});
+
+
+let totalCost = 0;
+/**
+ * Event listener for the change event on the activityRegister fieldset.
+ * Updates the total cost of selected activities and displays it.
+ * @param {Event} e - The event object.
+ */
+activityRegister.addEventListener('change', e => {
+    const activity = e.target;
+    const cost = parseInt(activity.dataset.cost);
+    if (activity.checked) {
+        totalCost += cost;
+    } else {
+        totalCost -= cost;
+    }
+    activitiesTotal.textContent = `Total: $${totalCost}`;
 });
